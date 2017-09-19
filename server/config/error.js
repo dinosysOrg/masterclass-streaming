4d419303d28965f9022e8ec7f534da0e5f732006
Sin <sin@Apples-MacBook-Pro.local>
2017-09-19 12:16:59 +0700

    fix bug and add docker compose

--- server/config/error.js
+++ server/config/error.js
@@ -2,5 +2,5 @@ module.exports = (code, message, next) => {
   let err = new Error();
   err.status = code;
   err.message = message;
-  next(err);
+  return next(err);
 };
--- server/middleware/secretkey_middleware.js
+++ server/middleware/secretkey_middleware.js
@@ -2,7 +2,7 @@ const secretKey = require('../config/secret');
 const error = require('../config/error');
 
 exports.checkSecretKey = (req, res, next) => {
-  let secretKeyHeader = req.body.secretkey;
+  let secretKeyHeader = req.get('secretkey');
   if ( secretKeyHeader != secretKey.secretCODE) {
     return error(405, 'You can not do this operation', next);
   }
