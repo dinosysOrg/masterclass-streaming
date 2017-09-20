39bb0462362b61c05c1a0a95bd4673f3a7cfa390
Sin <sin@Apples-MacBook-Pro.local>
2017-09-20 10:23:12 +0700

    add auto deploy after merged in github

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,7 @@ const uploadController = require('./controller/upload');
 const hostMiddleware = require('./middleware/host_middleware');
 const passportMiddleware = require('./middleware/passport_middleware');
 const secretKeyMiddleware = require('./middleware/secretkey_middleware');
+const systemController = require('./controller/system');
 
 module.exports = (app) => {
   let streamRoute = express.Router();
@@ -12,6 +13,7 @@ module.exports = (app) => {
   let apiKeyRoutes = express.Router();
   let uploadRouter = express.Router();
   let videoRouter = express.Router();
+  let systemRouter = express.Router();
   // Auth API
   authRoutes.post('/register', authenticationController.register);
   authRoutes.post('/login', passportMiddleware.requireLogin, authenticationController.login);
@@ -24,8 +26,10 @@ module.exports = (app) => {
   // Upload API
   uploadRouter.post('/upload', passportMiddleware.apiKeyAuthorization(['superuser']), uploadController.upload, uploadController.afterUploaded);
 
+  // System API
+  systemRouter.post('/webhook', systemController.autoDeploy);
   // Set up route
   app.use('/api/auth', authRoutes);
   app.use('/api/stream', streamRoute);
-  app.use('/api', [apiKeyRoutes, uploadRouter, videoRouter]);
+  app.use('/api', [apiKeyRoutes, uploadRouter, videoRouter, systemRouter]);
 };
